7f9e0545b97cc8aeb66e2fa708f3112e22c940ed
HypeKryzZ12 <vortexsweat69@outlook.com>
2025-10-18 18:21:01 -0400

    Delete Week2/hw2.py

--- Week2/hw2.py
+++
@@ -1,7 +0,0 @@
-from random import randint
-random_number = randint(0, 100)
-guess = int(input("What is the number??"))
-if guess == random_number:
-    print("YOU WIN!!!")
-else:
-    print("YOU LOSE :(")
