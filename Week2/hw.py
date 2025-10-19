32181fa378b115ecf33b01d5c9e6cf4bb63f9f6f
HypeKryzZ12 <vortexsweat69@outlook.com>
2025-10-19 17:29:13 -0400

    Update hw.py

--- Week2/hw.py
+++ Week2/hw.py
@@ -1,5 +1,6 @@
 from random import randint
-random_number = randint(0, 100)
+range = int(input("What range do you want it to be? (0, your number)"))
+random_number = randint(0, range)
 guess = int(input("What is the number??"))
 if guess == random_number:
     print("YOU WIN!!!")
