99a9d3cd67e05d147b308d5fdf05660938708599
HypeKryzZ12 <vortexsweat69@outlook.com>
2025-10-19 18:04:54 -0400

    Update hw.py

--- Week2/hw.py
+++ Week2/hw.py
@@ -1,8 +1,16 @@
 from random import randint
-range = int(input("What range do you want it to be? (0, your number)"))
-random_number = randint(0, range)
-guess = int(input("What is the number??"))
-if guess == random_number:
-    print("YOU WIN!!!")
-else:
-    print("YOU LOSE :(")
+ask_for_try_again = "yes"
+while ask_for_try_again == "yes":
+    range = int(input("What range do you want it to be? (0, your number)"))
+    random_number = randint(0, range)
+    guess = int(input("What is the number??"))
+    if guess == random_number:
+        print("YOU WIN!!!")
+        ask_for_try_again = "no"
+    else:
+        print("YOU LOSE :(")
+        ask_for_try_again = str(input("Do you want to try again? (yes or no)"))
+        if ask_for_try_again == "yes":
+            ask_for_try_again = "yes"
+        else:
+            ask_for_try_again = "no"
