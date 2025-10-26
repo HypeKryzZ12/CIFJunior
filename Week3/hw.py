07761ec4e98c34b7490941f2749a3fcf907efd55
HypeKryzZ12 <vortexsweat69@outlook.com>
2025-10-26 18:58:18 -0400

    Update hw.py
    Implement a number guessing game with retry logic.

--- Week3/hw.py
+++ Week3/hw.py
@@ -1,1 +1,24 @@
-
+from random import randint
+tries = 3
+ask_for_try_again = "yes"
+lowerbound = int(input("What range do you want it to be? (pls make it small)"))
+upperbound = int(input("What range do you want it to be? (pls make it bigger than last number)"))
+random_number = randint(lowerbound, upperbound)
+while ask_for_try_again == "yes" and tries > 0:
+    guess = int(input("What is the number??"))
+    if guess == random_number:
+        print("YOU WIN!!!")
+        ask_for_try_again = "no"
+    else:
+        print("YOU LOSE :(")
+        tries = tries - 1
+        if tries > 0:
+            if guess > random_number:
+                print("Number too big...")
+            if guess < random_number:
+                print("Number too small...")
+            ask_for_try_again = str(input("Do you want to try again? (yes or no)"))
+            if ask_for_try_again == "yes":
+                ask_for_try_again = "yes"
+            else:
+                ask_for_try_again = "no"
