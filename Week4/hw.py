1b1ae3500cdab77abbfe05babb47387ac4e43f2b
HypeKryzZ12 <vortexsweat69@outlook.com>
2025-11-09 16:55:24 -0500

    Update hw.py

--- Week4/hw.py
+++ Week4/hw.py
@@ -1,1 +1,13 @@
+weather = [
+    ["Sunday",[6, 7]],
+    ["Monday",[2, 5]],
+    ["Tuesday",[7, 10]],
+    ["Wednesday",[2, 6]],
+    ["Thursday",[4, 9]],
+    ["Friday",[1, 4]],
+    ["Saturday",[9, 11]],
+]
 
+print ("This is the weather forcast for this week:")
+for items in weather:
+    print (items)
