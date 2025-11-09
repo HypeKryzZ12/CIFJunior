62612d224c99edff2ec1e6c89697b9cb010b5f3b
HypeKryzZ12 <vortexsweat69@outlook.com>
2025-11-09 18:18:00 -0500

    Update hw2.py

--- Week4/hw2.py
+++ Week4/hw2.py
@@ -1,3 +1,3 @@
 def instruction():
-    print("Welcome to Math Debt \nAnswer math questions to get money \nIf you go in debt, you will lose \nGood luck!")
+    print("Welcome to Math Debt \nAnswer math questions to get money \nIf you go in debt, you will lose \nIf you reach a certain amount of money, you win \nGood luck!")
 instruction()
